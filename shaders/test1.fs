6f809b9992e98ec163eb6375fef5f9bf59a7b49b
alelievr <alelievr@student.42.fr>
2016-09-16 16:57:58 +0200

    added mirror shader

--- shaders/test1.fs
+++ shaders/test1.fs
@@ -1,4 +1,18 @@
-float iSphere(in vec3 ro, in vec3 rd, in vec4 sph)
+struct ray
+{
+	vec3 origin;
+	vec3 direction;
+};
+
+struct hit
+{
+	vec3 point;
+	vec3 normal;
+	float distance;
+	//material
+};
+
+void	iSphere(ray r, in vec4 sph, out hit h)
 {
     // a sphere centered at the origin has equation |xyz| = r
     // meaning, |xyz|^2 = r^2, meaning <xyz, xyz> = r^2
@@ -6,17 +20,19 @@ float iSphere(in vec3 ro, in vec3 rd, in vec4 sph)
 	// |rd| = 1 (normalized) so equation reduce to |ro|^2+ t^2 + 2<ro, rd> t - r^2 = 0
     // which is a quadratic equation, so
 
-    vec3 oc = ro - sph.xyz;
-    float b = 2.0 *dot(oc, rd);
+    vec3 oc = r.origin - sph.xyz;
+    float b = 2.0 *dot(oc, r.direction);
     float c = dot(oc, oc) - sph.w*sph.w;
-    float h = b*b - 4.0 *c;
-    if(h < 0.0)
-		return -1.0;
+    float h1 = b*b - 4.0 *c;
+    if(h1 < 0.0)
+		return ;
 
     //pick smaller one(i.e, close one)
     //not (-b+sqrt(h)) /2
-    float t = (-b - sqrt(h))/ 2.0;
-    return t;
+    float t = (-b - sqrt(h1))/ 2.0;
+	h.distance = t;
+	h.point = r.origin + r.direction * t;
+	h.normal = (h.point - sph.xyz) / sph.w * (sin(iGlobalTime) / 2 - .6);
 }
 
 void	mainImage(vec2 coord)
@@ -35,8 +51,14 @@ void	mainImage(vec2 coord)
 	vec3	up = normalize(cross(right, forw));
 	vec3	rd = normalize(uv.x * right + uv.y * up + fov * forw);
 
-	if (iSphere(cameraPos, rd, vec4(0, 0, -10, 1)) != -1)
-		fragColor = vec4(1, 1, 1, 1);
+	ray		camRay = ray(cameraPos, rd);
+	hit		h;
+
+	iSphere(camRay, vec4(0, 0, -10, 1), h);
+	if (h.distance != 0)
+	{
+		fragColor = vec4(h.normal, 1);
+	}
 	else
 		fragColor = vec4(0, 0, 0, 1);
 }
