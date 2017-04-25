fd9f74eeb3eb907554829ae11ff000f49bd95288
Antoine VIALLY <avially@e1r11p2.42.fr>
2017-04-25 05:41:46 +0200

    fucking recursion

--- shaders/tri/light.glsl
+++ shaders/tri/light.glsl
@@ -57,29 +57,8 @@ vec3		light(vec3 pos, Ray r, Hit h)
 	return (color * shadows(h.pos, d, h) + ambient);
 }
 
-/*Definition de la reflection*/
-vec3    freflection(vec3 pos, float reflection, Ray r, Hit h){
-  Hit   h2 = h;
-  float   on_off = 1;
-  int   i= -1;
-  vec3  color = vec3(0);
-  Ray   ref;
-
-	while (++i < 3 && on_off > 0)
-	{
-			h = h2;
-		  ref.dir = h.norm;
-			ref.pos = h.pos;
-			h2 = scene(ref);
-			reflection = atlas_fetch(h.mat.reflection, h.uv).x;
-			on_off = on_off * reflection;
-			color += light(pos, ref, h2) * on_off;
-	}
-  return (color);
-}
-
 /*Definition de la transparence*/
-vec3    fopacity(vec3 pos, float opacity, float refrac, Ray r, Hit h){
+vec3    fopacity(vec3 pos, float opacity, Ray r, Hit h){
   vec3  color = vec3(0);
   Ray   trans;
   float   on_off = 1;
@@ -99,6 +78,72 @@ vec3    fopacity(vec3 pos, float opacity, float refrac, Ray r, Hit h){
    return color;
 }
 
+
+
+/*Definition de la reflection*/
+vec3    freflection(vec3 pos, float reflection, Ray r, Hit h){
+  Hit   h2 = h;
+  float   on_off = 1;
+  int   i= -1;
+  vec3  color = vec3(0);
+  Ray   ref;
+	float opacity = 1;
+
+	while (++i < 3 && on_off > 0)
+	{
+			h = h2;
+		  ref.dir = h.norm;
+			ref.pos = h.pos;
+			h2 = scene(ref);
+			reflection = atlas_fetch(h.mat.reflection, h.uv).x;
+			on_off = on_off * reflection;
+			opacity = atlas_fetch(h2.mat.texture, h2.uv).w;
+			color += (fopacity(pos, opacity, ref, h2) + light(pos, ref, h2)) * on_off;
+	}
+  return (color);
+}
+
+vec3		fdeg(vec3 pos, float reflection, float opacity, Ray r, Hit h)
+{
+	Ray   ref;
+	Ray   trans;
+	Hit hre = h;
+	Hit htr = h;
+	Hit	hr;
+	Hit ht;
+	float   on_off_re = 1;
+	float		on_off_tr = 1;
+	vec3		color = vec3(0);
+	int			i = -1;
+
+	while((on_off_re > 0 || on_off_tr > 0) && ++i < 2)
+	{
+		reflection = atlas_fetch(hre.mat.reflection, hre.uv).x;
+		on_off_re = on_off_re * reflection;
+		if (on_off_re > 0)
+		{
+			hr = hre;
+			ref.dir = hr.norm;
+			ref.pos = hr.pos;
+			hre = scene(ref);
+			opacity = atlas_fetch(hre.mat.texture, hre.uv).w;
+			color += (fopacity(pos, opacity, ref, hre) + light(pos, ref, hre)) * on_off_re;
+		}
+		opacity = atlas_fetch(htr.mat.texture, htr.uv).w;
+		on_off_tr = on_off_tr * (1 - opacity);
+		if (on_off_tr > 0)
+		{
+			ht = htr;
+			trans.pos = ht.pos;
+			trans.dir = refraction(r.dir, ht.norm, atlas_fetch(ht.mat.refraction, ht.uv).x);
+			htr = scene(trans);
+			reflection = atlas_fetch(htr.mat.reflection, htr.uv).x;
+			color += (freflection(pos, reflection, trans, htr) + light(pos, trans, htr)) * on_off_tr;
+		}
+	}
+	return (color);
+}
+
 /* Définition des effets de lumiere */
 vec3		calc_light(vec3 pos, Ray r, Hit h)
 {
@@ -109,13 +154,16 @@ vec3		calc_light(vec3 pos, Ray r, Hit h)
 
   vec3  ref = vec3(0);
   vec3  tra = vec3(0);
+	vec3  deg = vec3(0);
 
 	if (bump != vec3(0))
 		h.norm  = normalize(bump + h.norm);
   vec3 lambert = light(pos, r, h) * opacity;
-  if (reflection > 0)
-    ref = freflection(pos, reflection, r, h);
-  if (opacity < 1)
-    tra = fopacity(pos, opacity, refrac, r, h);
-	return (lambert + tra + ref);
+  // if (reflection > 0)
+  //   ref = freflection(pos, reflection, r, h);
+  // if (opacity < 1)
+  //   tra = fopacity(pos, opacity, r, h);
+	if (reflection > 0 || opacity < 1)
+		deg += fdeg(pos, reflection, opacity, r, h);
+	return (lambert + deg);
 }
